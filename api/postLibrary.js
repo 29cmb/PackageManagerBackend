5441d1c49a0f3b85ebc37f03e3c162038ec44534
Cmb <71194682+29cmb@users.noreply.github.com>
2024-06-23 10:04:07 -0400

    Added post library functionality

---
+++ api/postLibrary.js
@@ -0,0 +1,39 @@
+const fs = require('fs')
+module.exports = function(app){
+    app.post("/postLib", (req, res) => {
+        var { body } = req
+        if (body == undefined) return;
+        var { Name, Author, Description, Serialized } = body
+
+        const unapprovedLibs = require("../unapprovedLibs.json")
+        const data = {
+            "ID": makeid(10),
+            "Name": Name,
+            "Author": Author,
+            "Description": Description,
+            "Downloads": 0,
+            "Serialized": JSON.parse(Serialized) // SHOULD be stringified (hopefully)
+        }
+
+        fs.writeFile("../unapprovedLibs.json", JSON.stringify(data, null, 2), (err) => {
+            if(err){
+                return console.error(`Error writing file: ${err}`)
+            }
+
+            console.log(`Added package '${Name}' by ${Author} to unapproved library list`)
+        })
+    })
+}
+
+// stack overflow my beloved
+function makeid(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+}
