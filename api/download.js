c8b3e3d24d74e8c347fb2281f63aa8c4899af767
Cmb <71194682+29cmb@users.noreply.github.com>
2024-06-23 14:41:51 -0400

    Added downloads api which increases "Downloads" by 1

---
+++ api/download.js
@@ -0,0 +1,31 @@
+const fs = require('fs')
+const path = require('path')
+module.exports = function(app){
+    app.post("/download", function(req, res){
+        const { body } = req
+        if(!body){
+            res.status(400).send("Body not provided")
+            return
+        }
+
+        const { ID } = body
+        if(!ID){
+            res.status(400).send("ID not provided")
+            return
+        }
+
+        const libs = require("../libs.json")
+        const item = libs.find(src => src.ID === ID)
+
+        if(!item){
+            res.status(404).send("Could not find item of the ID provided")
+            return;
+        }
+
+        item.Downloads++
+
+        fs.writeFileSync(path.join(__dirname, "../libs.json"), JSON.stringify(libs, null, 2))
+
+        res.send(item.Serialized)
+    })
+}
