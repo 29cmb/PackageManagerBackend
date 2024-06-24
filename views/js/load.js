9729e7945fbabb1afe8b62fc0e8e2ca54a5bc156
Cmb <71194682+29cmb@users.noreply.github.com>
2024-06-24 09:05:48 -0400

    Get library items and display them to the user, then provide them with a code to use.

---
+++ views/js/load.js
@@ -0,0 +1,37 @@
+function load(){
+    const container = document.getElementById("container")
+    fetch("/libs", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    }).then(response => response.json()).then(data => {
+        data.forEach(library => {
+            const libraryDiv = document.createElement('div');
+            libraryDiv.className = 'Library';
+
+            const titleElement = document.createElement('h4');
+            titleElement.id = 'Title';
+            titleElement.textContent = library.Name;
+
+            const descriptionElement = document.createElement('p');
+            descriptionElement.id = 'Description';
+            descriptionElement.textContent = library.Description;
+
+            const buttonElement = document.createElement('button');
+            buttonElement.id = 'Install';
+            buttonElement.innerHTML = "Install";
+            buttonElement.onclick = function(){
+                buttonElement.innerHTML = `Enter ID ${library.ID} in the <a href="https://example.com">roblox plugin</a> to install`
+            }
+
+            libraryDiv.appendChild(titleElement);
+            libraryDiv.appendChild(descriptionElement);
+            libraryDiv.appendChild(buttonElement);
+
+            container.appendChild(libraryDiv);
+        });
+    })
+}
+
+load()
